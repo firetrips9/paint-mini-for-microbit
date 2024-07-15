c1ee8dee00a1bef6c41c29abaac2b2085d5c7085
firetrips9 <83302743+firetrips9@users.noreply.github.com>
2024-07-15 20:32:07 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,22 @@
+input.onButtonPressed(Button.A, function () {
+    led.plot(ledindex, verticalledindex)
+    ledindex += 1
+    if (ledindex == 6) {
+        verticalledindex += 1
+        ledindex = 0
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    ledindex += 1
+    if (ledindex == 6) {
+        verticalledindex += 1
+        ledindex = 0
+    }
+})
+let verticalledindex = 0
+let ledindex = 0
+ledindex = 0
+verticalledindex = 0
+basic.forever(function () {
+	
+})
